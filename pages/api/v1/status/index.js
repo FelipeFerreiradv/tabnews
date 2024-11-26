2168ae6efc8d351eba16cefaaacee6c640b5de4d
Felipe Ferreira <fefelbf@gmail.com>
2024-11-26 01:21:36 +0000

    add: finish api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,33 @@
-import database from "infra/database.js";
+import database from "../../../../infra/database.js";
 
 const status = async (request, response) => {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseConnections = await database.query("SHOW max_connections;");
+  const databaseConnectionsValue =
+    await databaseConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    values: ["local_db"],
+  });
+  // "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnections.rows[0].count;
 
   response.status(200).json({
-    chave: "hello world",
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 };
 
